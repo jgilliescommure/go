e667cb017d0794f711653c67fcb6aab43f55d89a
Jason Gillies <jgillies@Jasons-MacBook-Pro.local>
2021-08-19 10:12:47 -0600

    first commit

---
+++ internal/service/personcrud.go
@@ -0,0 +1,28 @@
+package service
+
+import (
+	"context"
+
+	pb "personcrud/api/personcrud"
+)
+
+type PersoncrudService struct {
+	pb.UnimplementedPersoncrudServer
+}
+
+func NewPersoncrudService() *PersoncrudService {
+	return &PersoncrudService{}
+}
+
+func (s *PersoncrudService) CreatePerson(ctx context.Context, req *pb.CreatePersonRequest) (*pb.CreatePersonReply, error) {
+	return &pb.CreatePersonReply{Id: 11}, nil
+}
+func (s *PersoncrudService) UpdatePerson(ctx context.Context, req *pb.UpdatePersonRequest) (*pb.PersonReply, error) {
+	return &pb.PersonReply{}, nil
+}
+func (s *PersoncrudService) GetPerson(ctx context.Context, req *pb.GetPersonRequest) (*pb.PersonReply, error) {
+	return &pb.PersonReply{}, nil
+}
+func (s *PersoncrudService) ListPersons(ctx context.Context, req *pb.ListPersonRequest) (*pb.ListPersonReply, error) {
+	return &pb.ListPersonReply{}, nil
+}
